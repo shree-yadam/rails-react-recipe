d083bc968ae161b9f845cd200fe0167385b535ef
Shree Devi Yadam <shree.yadam@gmail.com>
2021-10-15 22:10:49 -0400

    display selected recipe or delte selected recipe controller

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -14,14 +14,25 @@ class Api::V1::RecipesController < ApplicationController
   end
 
   def show
+    if recipe
+      render json: recipe
+    else
+      render json: recipe.errors
+    end
   end
 
   def destroy
+    recipe&.destroy
+    render json: {message: 'Recipe deleted!'}
   end
 
   private
 
   def recipe_params
     params.permit(:name, :image, :ingredients, :instruction)
+  end
+
+  def recipe
+    @recipe ||= Recipe.find(params[:id])
   end
 end
